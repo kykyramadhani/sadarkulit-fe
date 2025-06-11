3a9527a2fbb1475d79161c4d422a656d0339c17f
Kisul12 <134827926+Kisul12@users.noreply.github.com>
2025-06-12 01:41:17 +0800

    npm run build

--- sadarKulit/src/components/Footer.jsx
+++ sadarKulit/src/components/Footer.jsx
@@ -24,7 +24,7 @@ export default function Footer() {
               <img src="/Logo.png" alt="SadarKulit Logo" className="h-12 w-auto" />
             </RouterLink>
             <p className="text-gray-400">
-              Membantu Anda mengenali kondisi kulit lebih dini menggunakan teknologi Machine Learning.
+              Membantu Anda mengenali kondisi kulit lebih dini menggunakan teknologi Berbasis Website & Machine Learning.
             </p>
           </div>
 
